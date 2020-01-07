69234316a5b42805f01e662435b37dc5a2294c74
Luke1803 <lkogenhop@gmail.com>
2020-01-07 13:53:38 +0100

    Hoofdstuk 2

--- src/H02/H2Opdracht5.java
+++ src/H02/H2Opdracht5.java
@@ -12,18 +12,33 @@ public class H2Opdracht5 {
         System.out.print("Typ een getal tussen 0 en 1000: ");
         int userInput = scanner.nextInt();
 
-        int rest = userInput % 10;
-        userInput = userInput / 10;
+        if (userInput >= 100 && userInput < 1000) {
+            int rest = userInput % 10;
+            userInput = userInput / 10;
 
-        int mid = userInput % 10;
-        userInput = userInput / 10;
+            int mid = userInput % 10;
+            userInput = userInput / 10;
 
-        int first = userInput;
+            int first = userInput;
 
-        int answer = first * mid * rest;
+            int answer = first * mid * rest;
 
-        System.out.println("De vermenigvuldiging van de cijfers luidt: " + first + " x " + mid + " x " + rest + " = " + answer);
+            System.out.println("De vermenigvuldiging van de cijfers luidt: " + first + " x " + mid + " x " + rest + " = " + answer);
+        } else if (userInput >= 10 && userInput < 100) {
+            int rest = userInput % 10;
+            userInput = userInput / 10;
 
+            int first = userInput;
+
+            int answer = first * rest;
+
+            System.out.println("De vermenigvuldiging van de cijfers luidt: " + first + " x " + " x " + rest + " = " + answer);
+
+        } else if (userInput >= 0 && userInput < 10) {
+            System.out.println("De vermenigvuldiging van de cijfers luidt: " + userInput + " = " + userInput);
+        } else {
+            System.out.println("sorry te groot getal");
+        }
 
 
     }
